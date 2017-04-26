d24a2a0f999442829fcdd2068ded4dff3d9d2513
James Mack <jmack@themxgroup.com>
2017-04-26 08:22:19 -0500

    Update some overall/dev settings

--- config/env/default.js
+++ config/env/default.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    title: 'Nationstates FT Galaxy Map',
+    description: 'An interactive, dynamic galaxy map for the NationStates Future Tech universe.',
+    keywords: 'nationstates, nation, states, future, FT, galaxy, galactic, map, maps, interactive, dynamic',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
   port: process.env.PORT || 3000,
